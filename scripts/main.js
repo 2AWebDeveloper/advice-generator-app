706e3b8756cd63bd8fedb67768d559259c4df789
Alireza Abolghasempour <161477999+2AWebDeveloper@users.noreply.github.com>
2025-02-01 17:41:26 +0330

    Upload project file ...

---
+++ scripts/main.js
@@ -0,0 +1,21 @@
+"use strict";
+
+const adviceID = document.querySelector(".advice-id");
+const adviceText = document.querySelector(".advice-text");
+const adviceBtn = document.querySelector(".advice-btn");
+
+const getAdvice = async function() {
+    try {
+        adviceBtn.setAttribute("disabled", true);
+        const response = await fetch("https://api.adviceslip.com/advice");
+        if (!response.ok) return;
+        const {slip: data} = await response.json();
+        adviceID.innerHTML = `ADVICE #${data.id}`;
+        adviceText.innerHTML = `“${data.advice}”`;
+    } catch(error) {
+        adviceText.innerHTML = "Sorry! Failed to fetch, Please try again later ...";
+    } finally {
+        adviceBtn.removeAttribute("disabled");
+    }
+};
+adviceBtn.addEventListener("click", getAdvice);
